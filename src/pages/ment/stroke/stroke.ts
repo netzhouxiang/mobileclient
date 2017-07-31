09523122c105d15035bd3c69b425ded6783297f1
周祥 <200592114@qq.com>
2017-08-01 03:10:11 +0800

    接入步骤参数提交

--- src/pages/ment/stroke/stroke.ts
+++ src/pages/ment/stroke/stroke.ts
@@ -15,32 +15,50 @@ import { HttpService } from "../../../providers/http.service";
 })
 export class StrokePage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams,public native: NativeService, private httpService: HttpService,) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public native: NativeService, private httpService: HttpService, ) {
   }
 
   ionViewDidLoad() {
     this.getpersonEvent();
     console.log('ionViewDidLoad StrokePage');
   }
-  strokeList=new Array();//事件列表
-  getpersonEvent(){//获取今日日程
-    let requestInfo={
-      url:"mobilegrid/getAllConcreteevent",
-      departmentID:this.native.UserSession.departments[0].department
+  strokeList = new Array();//事件列表
+  getpersonEvent() {//获取今日日程
+    let requestInfo = {
+      url: "mobilegrid/getAllConcreteevent",
+      departmentID: this.native.UserSession.departments[0].department
     }
-    this.httpService.post(requestInfo.url,requestInfo).subscribe(data=>{
-        try {
-          let res=data.json();
-          if(res.error){
-            this.native.showToast(res.error.error);
-          }else{
-            this.strokeList=res.success;
-          }
-        } catch (error) {
-          this.native.showToast(error);
+    this.httpService.post(requestInfo.url, requestInfo).subscribe(data => {
+      try {
+        let res = data.json();
+        if (res.error) {
+          this.native.showToast(res.error.error);
+        } else {
+          this.strokeList = res.success;
         }
-      },err=>{
-        this.native.showToast(err);
-      });
+      } catch (error) {
+        this.native.showToast(error);
+      }
+    }, err => {
+      this.native.showToast(err);
+    });
+  }
+  goOtherPage(obj) {//去其他页面
+    this.httpService.post('mobilegrid/getcurrentstep', { _id: obj._id, hideloading: true }).subscribe(data => {
+      try {
+        let res = data.json();
+        if (res.error) {
+          this.native.showToast(res.error.error);
+        } else {
+          let arr = res.success[res.success.length - 1];
+          this.navCtrl.push("stepPage", { "sid": arr._id, "eid": obj._id, "deptid": obj.department });
+        }
+      } catch (error) {
+        this.native.showToast(error);
+      }
+    }, err => {
+      this.native.showToast(err);
+    });
+
   }
 }
