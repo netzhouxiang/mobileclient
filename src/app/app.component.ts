ee54f8638165f8cbee259e8c978e945bd45b2962
周祥 <200592114@qq.com>
2018-03-08 01:02:19 +0800

    up

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -43,7 +43,7 @@ export class MyApp {
                 window.JMessage.init({ isOpenMessageRoaming: true })
             }
             //当前版本号
-            let curversion = "0.6.2";
+            let curversion = "0.6.3";
             //检测是否有更新 http://120.76.228.172/app/ver.json 
             httpService.get(nativeService.appServer.file + "app/ver.json").subscribe(data => {
                 var update_m = data.json();
